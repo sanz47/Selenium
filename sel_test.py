2ff8b18dbad59e68046fb321d4dcf09f6c2208ac
sanz47 <syemaziz@iut-dhaka.edu>
2023-08-02 02:46:18 +0600

    change in search prompt

--- sel_test.py
+++ sel_test.py
@@ -6,6 +6,6 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://google.com")
 search=driver.find_element(By.ID,"APjFqb")
-search.send_keys("Hello")
+search.send_keys("Adidas")
 search.send_keys(Keys.RETURN)
 time.sleep(20)
